4ae0215af605200071ac71e2ac60349342e5aa17
Chloe B <cbellas@ufl.edu>
2022-11-21 19:51:44 -0500

    Added filestream for data processing

--- main.cpp
+++ main.cpp
@@ -8,9 +8,37 @@
 /*  Description: TBD
 */
 
+#include <iostream>
+#include <fstream>
+#include <vector>
 #include "food.h"
+#include "processData.h"
 
 int main()
 {
+
+    int numFoodObj = 0;
+    string line;
+    fstream foodFile("food.csv", ios::in);
+    
+
+    if(foodFile.is_open())
+    {
+        // Remove the labels from the processed data
+        getline(foodFile, line);
+        while(getline(foodFile, line))
+        {
+
+            // Call processData here and assign to a food object
+            // Insert food Object into ordered map
+            
+        }
+        
+    }
+    else
+        cout << "Cannot Open File" << endl;
+
+   // Once all data has been processed and added to ordered map, provide user option between nutrient importance & BFS v. DFS
+
 
 }
---
+++ processData.h
@@ -0,0 +1,9 @@
+// Description: This function will read and process a singular line of food data and return a Food Object
+#pragma once
+#include "food.h"
+
+
+
+
+
+
